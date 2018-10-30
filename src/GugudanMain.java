0832311182fb24fc943e020da2d9a8ba96b7f7d5
Seong <tjdghkwnd1@naver.com>
2018-10-30 14:34:55 +0900

    Scanner가 어떤 객체인지 설명하는 주석기입

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -4,7 +4,7 @@ public class GugudanMain {
 	public static void main(String[] args) {
 		// 사용자에게 출력할 구구단 단수를 입력받는다
 		System.out.print("출력하고자 하는 구구단 단수는? :");
-		Scanner sr = new Scanner(System.in);
+		Scanner sr = new Scanner(System.in);  // Scanner는 사용자로부터 값을 입력받을 수 있는 클래스
 		int num = sr.nextInt();
 		int[] result = Gugudan.calculate(num);
 		Gugudan.print(result);
